3e01a0f9086c6a20dc558b27422af7554355fac0
FirePheonix <vlad@Vladimirs-MacBook-Pro.local>
2015-07-25 16:43:59 -0700

    field generation and function placeholders

---
+++ app/engine.js
@@ -0,0 +1,34 @@
+var engine = (function () {
+
+	var field = {
+		a: [],
+		b: [],
+		c: [],
+		d: [],
+		e: [],
+		f: [],
+		g: [],
+		h: []
+	};
+	var turn  = 'w'; // whose turn it is
+
+	return {
+		reset	: reset,
+		render	: render,
+		move	: move
+	}
+
+	function reset() {
+
+	}
+
+	function render() {
+
+	}
+
+	function move() {
+
+	}
+
+}());
+
