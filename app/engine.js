1a0844bddd23ef4c6d0545bf35ac09c4572cb4bb
Vitali Malinouski <vitmalina@gmail.com>
2015-07-25 22:29:40 -0700

    Started on pretend

--- app/engine.js
+++ app/engine.js
@@ -17,7 +17,10 @@ var engine = (function () {
 	return {
 		reset		: reset,
 		move		: move,
-		getMoves	: getMoves
+		getMoves	: getMoves,
+		pretend		: pretend,
+		isCheck		: isCheck,
+		show		: show
 	}
 
 	function reset() {
@@ -40,8 +43,8 @@ var engine = (function () {
 		$("#player1_move").html(engine.getMoves().length+" possible moves");
 	}
 
-	function move(action) {
-		var tmp = action.split(':');
+	function move(mv) {
+		var tmp = mv.split(':');
 		var f1  = tmp[0][0];
 		var i1  = parseInt(tmp[0][1]) - 1;
 		var f2  = tmp[1][0];
@@ -79,27 +82,27 @@ var engine = (function () {
 					if (piece[1] == 'p') {
 						// TODO: el pesan, pawn promotion
 						if (pl1 == 'w') {
-							if (i + 1 < 8 && field[f][i+1] == '') moves.push(f+(i+1)+':'+f+(i+2)); // one forward
-							if (i == 1 && field[f][i+1] == '' && field[f][i+2] == '') moves.push(f+(i+1)+':'+f+(i+3)); // two forward (if initial)
+							if (i + 1 < 8 && field[f][i+1] == '') addIfValid(piece, f, i, num, i+1); // one forward
+							if (i == 1 && field[f][i+1] == '' && field[f][i+2] == '') addIfValid(piece, f, i, num, i+2); // two forward (if initial)
 							if (num > 0 && i < 8) {
 								var beat = field[places[num-1]][i+1];
-								if (beat != '' && beat[0] == pl2) moves.push(f+(i+1)+':'+places[num-1]+(i+2)+':'+beat);
+								if (beat != '' && beat[0] == pl2) addIfValid(piece, f, i, num-1, i+1);
 							}
 							if (num < 7 && i < 8) {
 								var beat = field[places[num+1]][i+1];
-								if (beat != '' && beat[0] == pl2) moves.push(f+(i+1)+':'+places[num+1]+(i+2)+':'+beat);
+								if (beat != '' && beat[0] == pl2) addIfValid(piece, f, i, num+1, i+1);
 							}
 						}
 						if (pl1 == 'b') {
-							if (i - 1 >= 0 && field[f][i-1] == '') moves.push(f+(i+1)+':'+f+(i-0)); // one forward
-							if (i == 6 && field[f][i-1] == '' && field[f][i-2] == '') moves.push(f+(i+1)+':'+f+(i-1)); // two forward (if initial)
+							if (i - 1 >= 0 && field[f][i-1] == '') addIfValid(piece, f, i, num, i-1); // one forward
+							if (i == 6 && field[f][i-1] == '' && field[f][i-2] == '') addIfValid(piece, f, i, num, i-2); // two forward (if initial)
 							if (num > 0 && i > 0) {
 								var beat = field[places[num-1]][i-1];
-								if (beat != '' && beat[0] == pl2) moves.push(f+(i+1)+':'+places[num-1]+(i)+':'+beat);
+								if (beat != '' && beat[0] == pl2) addIfValid(piece, f, i, num-1, i-1);
 							}
 							if (num < 7 && i > 0) {
 								var beat = field[places[num+1]][i-1];
-								if (beat != '' && beat[0] == pl2) moves.push(f+(i+1)+':'+places[num+1]+(i)+':'+beat);
+								if (beat != '' && beat[0] == pl2) addIfValid(piece, f, i, num+1, i-1);
 							}
 						}
 					}
@@ -218,9 +221,40 @@ var engine = (function () {
 			if (beat && beat[0] == pl1) {
 				return false;
 			}
-			moves.push(f+(i+1)+':'+places[f1]+(i1+1)+(beat ? ':' + beat : ''));
+			var mv = f+(i+1)+':'+places[f1]+(i1+1)+(beat ? ':' + beat : '');
+			var fld = pretend($.extend(true, {}, field), mv);
+			// show(fld);
+			moves.push(mv);
 			return beat;
 		}
+	}
+
+	function pretend(field, mv) {
+		var tmp = mv.split(':');
+		var f1  = tmp[0][0];
+		var i1  = parseInt(tmp[0][1]) - 1;
+		var f2  = tmp[1][0];
+		var i2  = parseInt(tmp[1][1]) - 1;
+		field[f2][i2] = field[f1][i1];
+		field[f1][i1] = '';
+		return field;
+	}
+
+	function isCheck(fld) {
+		if (fld == null) fld = field;
+	}
+
+	function show(fld) {
+		if (fld == null) fld = field;
+		for (var i = 7; i >= 0; i--) {
+			var tmp = '';
+			for (var f in fld) {
+				if (fld[f][i] == '') tmp += ' . '; else tmp += fld[f][i]+'-';
+			}
+			console.log(i, tmp);
+		}
+		console.log('   a  b  c  d  e  f  g  h');
+		console.log('-------------------------');
 	}
 
 }());
