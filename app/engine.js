dfc00a4284f0aee489ba742f7ace6f9c1b61d3a4
FirePheonix <vlad@Vladimirs-MacBook-Pro.local>
2015-07-25 18:11:04 -0700

    Chess board gui and render function

--- app/engine.js
+++ app/engine.js
@@ -1,5 +1,7 @@
 var engine = (function () {
 
+	var places = 'abcdefgh';
+
 	var field = {
 		a: [],
 		b: [],
@@ -32,7 +34,60 @@ var engine = (function () {
 	}
 
 	function render() {
+		var html = "";
+		for (var i in field) {
+			for (var j = 0; j < 8; j++) {
 
+				var piece = field[i][j];
+
+				var color = "white";
+				var block = -1;
+				if (j%2) {
+					block = 1;
+				} else {
+					block = 0;
+				}
+				if ((places.indexOf(i)+block)%2) {
+					color = "#C3C4C5";
+				}
+
+				
+
+				if (piece) {
+
+					var left = 0;
+					var top = 0;
+
+					if (piece.substr(1,2) == "p") {
+						left = 11;
+						top = 7;
+					} else if (piece.substr(1,2) == "r") {
+						left = 9;
+						top = 7;
+					}  else if (piece.substr(1,2) == "h") {
+						left = 5;
+						top = 7;
+					} else if (piece.substr(1,2) == "b") {
+						left = 3;
+						top = 4;
+					} else if (piece.substr(1,2) == "q") {
+						left = 2;
+						top = 5;
+					} else if (piece.substr(1,2) == "k") {
+						left = 5;
+						top = 5;
+					}
+
+					html += "<div class='square' style='background-color: " + color + "; right: " + (places.indexOf(i)*64) + "px; bottom: " + (j*64) + "px'>"
+						+ "<img src='img/" + piece + ".png' style='left : " + left + "px; top: " + top + "px'>"
+						+ "</div>";	
+				} else {
+					html += "<div class='square' style='background-color: " + color + "; right: " + (places.indexOf(i)*64) + "px; bottom: " + (j*64) + "px'></div>";
+				}
+				
+			}
+		}
+		$("#board").html(html);
 	}
 
 	function move() {
