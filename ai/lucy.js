2e263c4c6489f84422cceb5fa8f8b01849b462bc
Vitali Malinouski <vitmalina@gmail.com>
2015-07-26 22:57:45 -0700

    Updated Lucy

--- ai/lucy.js
+++ ai/lucy.js
@@ -9,8 +9,19 @@ var ai = {
 	name	: 'Lucy',
 	author	: 'Vitali Malinouski',
 	next	: function (field, moves) {
+		var tmp = moves[0].split(':')[0];
+		var my  = field[tmp[0]][parseInt(tmp[1])-1][0]; // my color
+		var op  = (my == 'w' ? 'b' : 'w');			    // oponent color
 		// get random move
 		var index = Math.floor(Math.random() * moves.length);
+		// score moves
+		for (var i = 0; i < moves.length; i++) {
+			var move = moves[i].split(':');
+			if (move.length == 3) {
+				index = i;
+				break;
+			}
+		}
 		return moves[index];
 	}
 }
