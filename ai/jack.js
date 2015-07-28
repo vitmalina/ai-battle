3deeed593cba69438e08020a92e2620f60ce2ce8
FirePheonix <aleximonkov@gmail.com>
2015-07-28 15:57:55 -0700

    Andi Update

--- ai/jack.js
+++ ai/jack.js
@@ -10,33 +10,74 @@
  	///// Manditory Info /////
 	name	: 'Jack',
 	author	: 'Kirill Shpin',
-
-
+	
 	next	: function (field, moves) {
-		var myColor = "w";
-		var oppColor = "b";
-		var chosen = null;
-
-		for (var num in moves) {
-			var tmp = moves[num].split(":");
-			if (tmp[2]) chosen = moves[num];
+		if (!ai.me) {
+			ai.me = field[moves[0][0]][parseInt(moves[0][1])][0];
+			ai.enemy = ai.me == "w" ? "b" : "w";
 		}
 
-		var most = 0;
-		if (!chosen) {
-			for (var num in moves) {
-				var newField = pretendMove(moves[num]);
-				var newFieldMoves = getMoves(newField, myColor).length;
+		var myMove = null;
+		var enemyMove = null;
+		var modField = field;
+		var modMoves = moves;
+		// for (var i in moves) {
+		// 	myMove = ai.think(
+		// 		field,
+		// 		moves
+		// 	);
+		// 	modField = pretendMove(
+		// 		field,
+		// 		myMove
+		// 	);
+		// 	modMoves = getMoves(
+		// 		modField,
+		// 		ai.enemy
+		// 	);
+			
+		// 	enemyMove = ai.think(
+		// 		modField,
+		// 		modMoves
+		// 	);
+		// 	if (!enemyMove.split(":")[2]) break;
+		// }
 
-				if (most < newFieldMoves) {
-					most = newFieldMoves;
-					chosen = moves[num];
+		// /*var myNextMove = pretendMove(
+		// 	enemyMove,
+		// 	ai.think(
+		// 		enemyMove,
+		// 		getMoves(
+		// 			enemyMove,
+		// 			ai.me
+		// 		)
+		// 	),
+		// 	ai.me
+		// );*/
+
+		if (!myMove) myMove = ai.think(field, moves);
+
+		return myMove;
+	},
+
+	think	: function (field, moves) {
+		var chosen = null;
+		for (var num in moves) {
+			var tmp = moves[num].split(":");
+			if (tmp[2]) {
+				switch (tmp[2][1]) {
+					case "p": chosen = num; break;
+					case "h": chosen = num; break;
+					case "b": chosen = num; break;
+					case "r": chosen = num; break;
+					case "q": chosen = num; break;
 				}
 			}
 		}
 
-		//console.log(moves);
-
-		return chosen;
-	},
+		var most = 0;
+		if (!chosen) {
+			chosen = Math.floor(Math.random()*moves.length);
+		}
+		return moves[chosen];
+	}
 }
