ca4b9f13a6090e50fca591165ad5318309890918
FirePheonix <aleximonkov@gmail.com>
2015-07-28 14:47:09 -0700

    Andi Update

--- ai/andi.js
+++ ai/andi.js
@@ -12,7 +12,8 @@
 	author	: 'Vladimir Malinouski',
 
 
-	color : '',
+	color     : '',
+	lastMoves : [],
 
 
 	next	: function (field, moves) {
@@ -22,33 +23,49 @@
 		var places = "abcdefgh";
 
 		if (!ai.color) {
-			ai.color = field[moves[0].substr(0,1)][parseInt(moves[0].substr(1,1))].substr(0,1);
+			ai.color = field[moves[0].substr(0,1)][parseInt(moves[0].substr(1,1))-1].substr(0,1);
+			ai.op    = (ai.color == 'w' ? 'b' : 'w');
 			console.log(ai.color);
 		}
 
 		for (var i = 0; i < moves.length; i++) {
 
 			/////  Available Moves /////
-			var fieldInstance = pretendMove(field, moves[i]);
-			var availMoves = getMoves(fieldInstance, ai.color).length;
+			var fieldInstance = pretendMove($.extend(true, {}, field), moves[i]);
+			var availMoves = getMoves($.extend(true, {}, fieldInstance), ai.color).length;
 
 
 			///// Takeable Piece /////
 			var takeablePiece = "";
 			var split = moves[i].split(":");
 			if (split[2]) {
-				takeablePiece = split[2];
+				takeablePiece = split[2].substr(1,1);
+				//console.log(takeablePiece);
 			} 
+			var prot = false;
+			var killingMoves = getMoves($.extend(true, {}, fieldInstance), ai.op);
+			for (var j = 0; j < killingMoves.length; j++) {
+				var spl = killingMoves[j].split(":");
+				if (spl[1] == split[1]) {
+					prot = true;
+				}
+			}
 
 			///// Piece /////
-			var piece = field[moves[i].substr(0,1)][parseInt(moves[i].substr(1,1))];
+			var tmp = parseInt(moves[i].substr(1,1));
+			tmp = tmp -1;
+			var piece = field[moves[i].substr(0,1)][tmp].substr(1,1);
+			console.log(piece);
 
 
 
 			moveStats.push({
 
 				availMoves     : availMoves,
-				takeablePiece : takeablePiece,
+				take  : {
+					piece : takeablePiece,	
+					prot  : prot,
+				},
 
 				move   : moves[i],
 				piece  : piece,
@@ -59,34 +76,49 @@
 
 		}
 
-		//console.log(moveStats);
+
 
 		
 		chosen = ai.totalPoints(moveStats);
+
+
+		//console.log(chosen);
+		//console.log(moveStats);
+
+		ai.lastMoves = moves;
 
 		return chosen;
 	},
 
 
 	totalPoints : function(moves) {
-		var chosen  = -1;
+		var chosen  = moves[0].move;
 		var biggest = 0;
 
 		for (var i = 0; i < moves.length; i++) {
-			// if (moves[i].availMoves > biggest) {
-			// 	biggest = moves[i].availMoves;
-			// 	chosen = moves[i].move;
-			// }
-			if (moves[i].takeablePiece != "") {
-				chosen = moves[i].move;
-			}
-		}
-		if (chosen == -1) {
-			for (var i = 0; i < moves.length; i++) {
-				if (moves[i].availMoves > biggest) {
-					biggest = moves[i].availMoves;
-					chosen = moves[i].move;
+			if (moves[i].take.piece != "") {
+				var p = {
+					p : 3,
+					h : 8,
+					b : 9,
+					r : 20,
+					q : 31,
+				};
+				if (!moves[i].take.prot) {
+					moves[i].points += p[moves[i].take.piece];
+				} else {
+					moves[i].points -= p[moves[i].piece];
 				}
+			}
+			if (moves[i].availMoves > ai.lastMoves.length && moves[i].piece != "k") {
+				moves[i].points += (moves[i].availMoves/50);
+			}
+			
+		}
+		for (var i = 0; i < moves.length; i++) {
+			if (moves[i].points >= biggest) {
+				biggest = moves[i].points;
+				chosen = moves[i].move;
 			}
 		}
 
